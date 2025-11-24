1962cc5f5607de61ddbba7af0464b41fafdb1c46
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-25 02:05:12 +0330

    improve a11y on pray button

--- client-web/src/App.vue
+++ client-web/src/App.vue
@@ -18,8 +18,8 @@
     <div class="flex-1 flex flex-col items-center justify-center">
       <h1 class="text-3xl font-bold">صلوات مدرسه</h1>
       <p>اینجا میتونی همراه با سایر دانش آموزان ایران بشینی و برای تعطیلی مدارس صلوات بفرستی :)</p>
-      <button @click="clicks.click()" class="m-4 p-8 rounded-full cursor-pointer hover:opacity-90 active:scale-110 border border-slate-300 bg-slate-50">
-        <img src="/pray.png" alt="دکمه صلوات" width="64px" height="64px">
+      <button @click="clicks.click()" aria-label="دکمه صلوات فرستادن" class="m-4 p-8 rounded-full cursor-pointer hover:opacity-90 active:scale-110 border border-slate-300 bg-slate-50">
+        <img src="/pray.png" width="64px" height="64px">
       </button>
       <div v-if="!clicks.loading">
         <h2 class="flex gap-1 text-xl font-semibold">
