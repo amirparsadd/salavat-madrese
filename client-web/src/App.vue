7709f676d5b6a9546f5c4e2132945b8300b0636f
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-25 18:31:41 +0330

    added transition on clicker button

--- client-web/src/App.vue
+++ client-web/src/App.vue
@@ -20,7 +20,7 @@
     <div class="flex-1 flex flex-col items-center justify-center">
       <h1 class="text-3xl font-bold">صلوات مدرسه</h1>
       <p>اینجا میتونی همراه با سایر دانش آموزان ایران بشینی و برای تعطیلی مدارس صلوات بفرستی :)</p>
-      <button @click="clicks.click()" aria-label="دکمه صلوات فرستادن" class="m-4 p-8 rounded-full cursor-pointer hover:opacity-90 active:scale-110 border border-slate-300 bg-slate-50">
+      <button @click="clicks.click()" aria-label="دکمه صلوات فرستادن" class="m-4 p-8 rounded-full cursor-pointer transition-all duration-200 hover:opacity-90 active:scale-110 border border-slate-300 bg-slate-50">
         <img src="/pray.png" width="64px" height="64px">
       </button>
       <div v-if="!clicks.loading">
