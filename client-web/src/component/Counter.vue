7c328ddb05012bf43b80f9d5892a3efc498cb943
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-02 20:48:01 +0330

    fix spacing counter component

--- client-web/src/component/Counter.vue
+++ client-web/src/component/Counter.vue
@@ -1,41 +1,41 @@
 <script setup lang="ts">
-import { ref, watch, onMounted } from 'vue';
-import { CountUp } from 'countup.js';
+  import { ref, watch, onMounted } from 'vue';
+  import { CountUp } from 'countup.js';
 
-interface Props {
-  value: number;
-}
-
-const props = defineProps<Props>();
-const counterRef = ref<HTMLElement | null>(null);
-let countUpInstance: CountUp;
-
-onMounted(() => {
-  if (counterRef.value) {
-    const duration = Math.min(2, Math.log10(props.value + 1) * 0.3);
-    countUpInstance = new CountUp(counterRef.value, props.value, {
-      duration: duration,
-      formattingFn: n => n.toLocaleString("fa-IR"),
-      easingFn: (t, b, c, d) => {
-        const progress = t / d;
-        const smallIncrement = c === 1;
-        
-        // if the increment is tiny, skip most of the animation
-        if (smallIncrement) {
-          return b + c
-        }
-
-        // normal easeOutQuart for larger jumps
-        return b + c * (1 - Math.pow(1 - progress, 4));
-      }
-    });
-    countUpInstance.start();
+  interface Props {
+    value: number;
   }
-});
 
-watch(() => props.value, (newVal) => {
-  countUpInstance?.update(newVal);
-});
+  const props = defineProps<Props>();
+  const counterRef = ref<HTMLElement | null>(null);
+  let countUpInstance: CountUp;
+
+  onMounted(() => {
+    if (counterRef.value) {
+      const duration = Math.min(2, Math.log10(props.value + 1) * 0.3);
+      countUpInstance = new CountUp(counterRef.value, props.value, {
+        duration: duration,
+        formattingFn: n => n.toLocaleString("fa-IR"),
+        easingFn: (t, b, c, d) => {
+          const progress = t / d;
+          const smallIncrement = c === 1;
+          
+          // if the increment is tiny, skip most of the animation
+          if (smallIncrement) {
+            return b + c
+          }
+
+          // normal easeOutQuart for larger jumps
+          return b + c * (1 - Math.pow(1 - progress, 4));
+        }
+      });
+      countUpInstance.start();
+    }
+  });
+
+  watch(() => props.value, (newVal) => {
+    countUpInstance?.update(newVal);
+  });
 </script>
 
 <template>
