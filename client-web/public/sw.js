ba8e29f3d7309486778bf662edcbf6e2e610df04
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-02 19:59:12 +0330

    add offline support to service worker

--- client-web/public/sw.js
+++ client-web/public/sw.js
@@ -1,7 +1,39 @@
+const CACHE_NAME = "salavat-madrese-cache-v1";
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  const url = new URL(request.url);
+  const isAppEndpoint = url.origin === self.location.origin;
+
+  if (!isAppEndpoint) return;
+
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      const cachedResponse = await cache.match(request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+      } catch (err) {
+        if (cachedResponse) return cachedResponse;
+        return new Response("You are offline", { status: 503 });
+      }
+    })()
+  );
 });
