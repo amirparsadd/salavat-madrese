e56ef70e638672571df811f92e963d985d8bb7e5
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-03 21:47:00 +0330

    add pray button component

---
+++ client-web/src/component/PrayButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import { clicks } from '../state/clicks';
+</script>
+
+<template>
+  <button @click="clicks.click()" aria-label="دکمه صلوات فرستادن" class="m-4 p-8 rounded-full cursor-pointer transition-all duration-200 hover:opacity-90 active:scale-110 border border-slate-300 bg-slate-50">
+    <img src="/pray.png" width="64px" height="64px">
+  </button>
+</template>
