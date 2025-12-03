1407d60617d3f4384010af6a2191ed89f36cd565
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-03 21:51:04 +0330

    better structure on getIp function

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -2,5 +2,7 @@ import { getConnInfo } from "@hono/node-server/conninfo";
 import type { Context, Env, Input } from "hono";
 
 export function getIp(c: Context<Env, string, Input>) {
-  return c.req.header()["ar-real-ip"] ?? c.req.header()["x-forwarded-for"] ?? String(getConnInfo(c).remote.address)
+  return c.req.header()["ar-real-ip"]
+    ?? c.req.header()["x-forwarded-for"]
+    ?? String(getConnInfo(c).remote.address)
 }
