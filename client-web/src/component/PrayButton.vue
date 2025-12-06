95f120e1e7824115191136742249b19236d6430f
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-06 15:43:02 +0330

    feat(a11y): pray button image alt text

--- client-web/src/component/PrayButton.vue
+++ client-web/src/component/PrayButton.vue
@@ -4,6 +4,11 @@
 
 <template>
   <button @click="clicks.click()" aria-label="دکمه صلوات فرستادن" class="m-4 p-8 rounded-full cursor-pointer transition-all duration-200 hover:opacity-90 active:scale-110 border border-slate-300 bg-slate-50">
-    <img src="/pray.png" width="64px" height="64px">
+    <img
+      src="/pray.png"
+      width="64px"
+      height="64px"
+      alt="Pray Button Image"
+    >
   </button>
 </template>
