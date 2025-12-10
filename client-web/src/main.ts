0ea98ceac3e77f6c473e135cce955b3894d1e8ec
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-10 21:32:13 +0330

    refactor(sw): use async for service worker registration

--- client-web/src/main.ts
+++ client-web/src/main.ts
@@ -12,13 +12,12 @@ app.mount('#app')
 
 // Register service worker
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        console.log('Service Worker registered:', registration);
-      })
-      .catch((error) => {
-        console.log('Service Worker registration failed:', error);
-      });
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js') 
+      console.log('Service Worker registered:', registration);
+    } catch (error) {
+      console.log('Service Worker registration failed:', error);
+    }
   });
 }
